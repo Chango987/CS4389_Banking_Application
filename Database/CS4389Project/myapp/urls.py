b2012b93dd9eaae2eda25b5a5bc2443c075ed6d5
josetorres <77871017+josetorres222@users.noreply.github.com>
2024-11-26 14:42:34 -0600

    Update urls.py

--- Database/CS4389Project/myapp/urls.py
+++ Database/CS4389Project/myapp/urls.py
@@ -1,7 +1,12 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='member'),
-    path('join/', views.join, name='join')
+    path('login/', views.login, name='login'),
+    path('api/account/', views.get_account_data, name='get_account_data'),  # Account data endpoint
+    path('api/transactions/', views.transaction_list, name='transaction_list'),  # Transactions list endpoint
+    path('api/user/', views.user_info, name='user_info'),  # User info endpoint
+    path('api/banks/', views.bank_list, name='bank_list'),  # Bank list endpoint
+    path('api/accounts/', views.account_list, name='account_list'),  # Accounts list endpoint
 ]
