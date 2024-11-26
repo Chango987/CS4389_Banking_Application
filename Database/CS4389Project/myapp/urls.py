8023562991a9315b298d78326aa671b81b25c4b5
josetorres <77871017+josetorres222@users.noreply.github.com>
2024-11-26 14:05:58 -0600

    Update urls.py

--- Database/CS4389Project/myapp/urls.py
+++ Database/CS4389Project/myapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('api/account/', views.get_account_data, name='get_account_data'),  # New data endpoint
     path('api/transactions/', views.get_transactions, name='get_transactions'), 
+    path('api/user/', views.user_info, name='user-info'),
+    path('api/banks/', views.bank_list, name='bank-list'),
 ]
