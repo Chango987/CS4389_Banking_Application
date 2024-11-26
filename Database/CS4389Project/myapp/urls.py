f78c90aa3897cb5599edd4f0ed20655089cdedeb
josetorres <77871017+josetorres222@users.noreply.github.com>
2024-11-26 13:59:59 -0600

    Update urls.py

--- Database/CS4389Project/myapp/urls.py
+++ Database/CS4389Project/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('api/account/', views.get_account_data, name='get_account_data'),  # New data endpoint
 ]
