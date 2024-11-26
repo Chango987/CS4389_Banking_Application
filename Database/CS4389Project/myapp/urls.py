ddf9a99ec6b50b10830c6d38b09e557f2a4126bc
josetorres <77871017+josetorres222@users.noreply.github.com>
2024-11-26 14:08:06 -0600

    Update urls.py

--- Database/CS4389Project/myapp/urls.py
+++ Database/CS4389Project/myapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('api/transactions/', views.get_transactions, name='get_transactions'), 
     path('api/user/', views.user_info, name='user-info'),
     path('api/banks/', views.bank_list, name='bank-list'),
+    path('api/accounts/', views.account_list, name='account-list'),
+    path('api/transactions/', views.transaction_list, name='transaction-list'),
 ]
